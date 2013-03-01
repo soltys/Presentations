c758e20d05f953de0b4c2f1ac014741a1ad06e03
Paweł Sołtysiak <soltys@uznam.org>
2013-03-01 13:35:50 +0100

    Created build script

---
+++ Windows 8 - Pracownia dyplomowa/pdf.bat
@@ -0,0 +1,10 @@
+@echo off
+rem skrypt do generacji doumentu PDF
+rem wygenerowanie pierwszego pliku aux
+pdflatex -file-line-error-style -output-directory=tmp -aux-directory=tmp -include-directory=tex tex/presentation.tex
+pdflatex -file-line-error-style -output-directory=tmp -aux-directory=tmp -include-directory=tex tex/presentation.tex
+rem tworzenie odwo³añ do bibliografii
+
+
+rem i interaktywny pdf gotowy
+move tmp\presentation.pdf Windows8_Windows_Runtime_presentation.pdf
